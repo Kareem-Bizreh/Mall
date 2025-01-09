ca668fb55e6122151a98270ddbdeced818366127
Abdalrahim <abdsarhan@gmail.com>
2025-01-09 13:46:17 +0300

    add lake

--- Mall_Project/SuperMarket.cpp
+++ Mall_Project/SuperMarket.cpp
@@ -10,7 +10,7 @@
 
 void SuperMarket::loadTextures()
 {
-	roof.loadTexture("textures\\SuperMarket\\roof.jpg");
+	/*roof.loadTexture("textures\\SuperMarket\\roof.jpg");
 	backWall.loadTexture("textures\\SuperMarket\\all.jpg");
 	floor.loadTexture("textures\\SuperMarket\\floor3.jpg");
 	products.loadTexture("textures\\SuperMarket\\products.jpg");
@@ -24,7 +24,11 @@ void SuperMarket::loadTextures()
 	friends.loadTexture("textures\\SuperMarket\\HelloWorld.jpg");
 	discounts.loadTexture("textures\\SuperMarket\\discounts.jpg");
 	marketImage.loadTexture("textures\\SuperMarket\\MarketLogo.jpg");
-	welcome.loadTexture("textures\\SuperMarket\\welcome.png");
+	welcome.loadTexture("textures\\SuperMarket\\welcome.png");*/
+	floorLake.loadTexture("textures\\SuperMarket\\floorLake2.jpg");
+	lakeWall.loadTexture("textures\\SuperMarket\\lakeWall5.jpg");
+	partie.loadTexture("textures\\SuperMarket\\parties.jpg");
+
 }
 
 void SuperMarket::drawTable(Point center) {
@@ -120,6 +124,86 @@ void SuperMarket::drawLight()
 	glColor3f(1, 1, 1);
 }
 
+void SuperMarket::drawParties()
+{
+	Cuboid parties(Point(0, 0, 0), 11, 5, 5), upParties(Point(0, 11, 0), 2, 7, 7);
+
+	glColor3ub(255, 255, 255);
+	parties.drawWithTexture(lakeWall.textureID, 1, 1);
+	upParties.drawWithTexture(partie.textureID,1,1);
+}
+
+void SuperMarket::drawLake()
+{
+	GLUquadric* quad = gluNewQuadric();
+	glPushMatrix();
+	glTranslated(0, 0, 0);
+	this->drawParties();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(50, 0, 0);
+	this->drawParties();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(0, 0, -50);
+	this->drawParties();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(50, 0, -50);
+	this->drawParties();
+	glPopMatrix();
+
+	Cuboid left(Point(0, 0, -25), 10, 45,5), right(Point(50, 0, -25), 10, 45, 5);
+	Cuboid top(Point(25, 0, -50), 10, 5, 45), bottom(Point(25, 0, 0), 10, 5, 45);
+	Cuboid center(Point(25, 0, -25), 12, 5, 5), floor(Point(25, -0.2, -25), 0.2, 50, 50);
+	Cuboid topOfCenter(Point(25, 12.1, -25), 0.2, 10, 10);
+
+	glColor3ub(255, 255, 255);
+
+	left.drawWithTexture(lakeWall.textureID, 10, 1);
+	right.drawWithTexture(lakeWall.textureID, 10, 1);
+	top.drawWithTexture(lakeWall.textureID, 10, 1);
+	bottom.drawWithTexture(lakeWall.textureID, 10, 1);
+
+	floor.drawWithTexture(floorLake.textureID, 1, 1);
+	
+	center.drawWithTextureNoTopBottom(lakeWall.textureID, 1, 1);
+
+	glColor3f(0.5, 0.5, 0.5);
+	topOfCenter.draw();
+
+	glColor3f(0, 0, 0);
+	glPushMatrix();
+	glTranslatef(25,12.2,-25);
+	glRotated(-90, 1, 0, 0);
+	gluCylinder(quad, 3, 3, 2, 32, 32);
+	glPopMatrix();
+
+	glColor3f(0, 0, 0);
+	glPushMatrix();
+	glTranslatef(25, 12.3, -25);
+	glRotated(-90, 1, 0, 0);
+	gluCylinder(quad, 3, 4, 2, 32, 32);
+	glPopMatrix();
+
+	glColor3f(0, 0, 0);
+	glPushMatrix();
+	glTranslatef(25, 14.2, -25);
+	glRotated(-90, 1, 0, 0);
+	gluCylinder(quad, 1.5, 2.5, 1, 32, 32);
+	glPopMatrix();
+
+	glColor3f(0, 0, 0);
+	glPushMatrix();
+	glTranslatef(25, 12.2, -25);
+	glRotated(-90, 1, 0, 0);
+	gluCylinder(quad, 0.1, 0.1, 5, 32, 32);
+	glPopMatrix();
+}
+
 
 void SuperMarket::draw()
 {
@@ -202,3 +286,5 @@ void SuperMarket::draw()
 	glPopMatrix();
 
 }
+
+
